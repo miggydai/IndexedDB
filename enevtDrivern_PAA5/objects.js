943d0a45fe37b7c9830e604abc76841b51db6007
Migs <miguelemmanueldailisan@gmail.com>
2022-03-14 17:28:09 +0800

    main
    main folder

---
+++ enevtDrivern_PAA5/objects.js
@@ -0,0 +1,35 @@
+
+let objects = [
+    { price: 350, name: "Baked salmon with fennel & tomatoes", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/1548772327.jpg", id: "52959" },
+    { price: 250, name: "Cajun spiced fish tacos", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/uvuyxu1503067369.jpg", id: "52819" },
+    { price: 300, name: "Escovitch Fish", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/1520084413.jpg", id: "52944" },
+    { price: 380, name: "Fish fofos", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/a15wsa1614349126.jpg", id: "53043" },
+    { price: 190, name: "Fish pie", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/ysxwuq1487323065.jpg", id: "52802" },
+    { price: 190, name: "Fish Stew with Rouille", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/vptqpw1511798500.jpg", id: "52918" },
+    { price: 220, name: "Garides Saganaki", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/wuvryu1468232995.jpg", id: "52764" },
+    { price: 350, name: "Grilled Portuguese sardines", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/lpd4wy1614347943.jpg", id: "53041" },
+    { price: 680, name: "Honey Teriyaki Salmon", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/xxyupu1468262513.jpg", id: "52773" },
+    { price: 500, name: "Kedgeree", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/utxqpt1511639216.jpg", id: "52887" },
+    { price: 420, name: "Kung Po Prawns", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/1525873040.jpg", id: "52946" },
+    { price: 390, name: "Laksa King Prawn Noodles", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/rvypwy1503069308.jpg", id: "52821" },
+    { price: 360, name: "Mediterranean Pasta Salad", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/wvqpwt1468339226.jpg", id: "52777" },
+    { price: 375, name: "Mee goreng mamak", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/xquakq1619787532.jpg", id: "53048" },
+    { price: 375, name: "Nasi lemak", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/wai9bw1619788844.jpg", id: "53051" },
+    { price: 360, name: "Portuguese fish stew (Caldeirada de peixe)", image: "https:\/\/www.themealdb.com\/images\/media\/meals\/do7zps1614349775.jpg", id: "53045" },
+    ]
+
+    //var MyIndex = objects.name
+  //  for (i of objects){
+   //     var MyIndex = i.id
+   //     console.log(MyIndex)
+   //    }
+          
+
+
+
+//function test(){
+//    db.collection('items').add(objects[0]) 
+//}
+
+
+//console.log(objects[0].price);
